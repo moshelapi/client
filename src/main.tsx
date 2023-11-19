8f7bca26569f65dc6449522e9bbec420f6d5415c
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-20 00:19:12 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/moshelapi/client.git
+git push -u origin main
